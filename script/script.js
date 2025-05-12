06c6fa38377e02e403138f12f13479c31e7a6f17
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-05-12 10:49:05 +0900

    비밀번호 예시 1234

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
 
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function() {
 
-})
-
+    let password;
+    while (true) {
+        password = prompt("비밀번호를 입력해주세요", "");
+        if (password === "1234") {
+            break; // 비밀번호가 맞으면 반복 종료
+        } else {
+            alert("비밀번호가 틀렸습니다. 다시 시도해주세요.");
+        }
+    }
+});
 
